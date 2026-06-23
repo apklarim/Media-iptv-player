81441cd89353a733630928a588924bf1565a9918
apklarim <apklarim@gmail.com>
2026-06-23 15:33:49 +0300

    Update MainActivity.kt

--- app/src/main/java/com/media/iptvplayer/MainActivity.kt
+++ app/src/main/java/com/media/iptvplayer/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.media.iptvplayer
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.btnSettings)
-            .setOnClickListener {
-
-                startActivity(
-                    Intent(this, SettingsActivity::class.java)
-                )
-            }
     }
 }

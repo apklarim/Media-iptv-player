f67334b29c89facf6716221fbeb4924ac10af746
apklarim <apklarim@gmail.com>
2026-06-24 00:11:32 +0300

    Update ChannelListActivity.kt

--- app/src/main/java/com/media/iptvplayer/ChannelListActivity.kt
+++ app/src/main/java/com/media/iptvplayer/ChannelListActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ChannelListActivity : AppCompatActivity() {
@@ -21,30 +20,17 @@ class ChannelListActivity : AppCompatActivity() {
         val listView =
             findViewById<ListView>(R.id.listChannels)
 
-        val playlistName =
+        title.text =
             intent.getStringExtra("playlistName")
                 ?: "Kanal Listesi"
 
-        title.text = playlistName
-
-        if (ChannelRepository.channels.isEmpty()) {
-
-            Toast.makeText(
-                this,
-                "Henüz kanal yüklenmedi",
-                Toast.LENGTH_LONG
-            ).show()
-
-            return
-        }
-
-        val channelNames =
+        val names =
             ChannelRepository.channels.map { it.name }
 
         listView.adapter = ArrayAdapter(
             this,
             android.R.layout.simple_list_item_1,
-            channelNames
+            names
         )
     }
 }

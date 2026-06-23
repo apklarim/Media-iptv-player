eeda39f7438b23f9ae095d51c9ccac07f343b4e7
apklarim <apklarim@gmail.com>
2026-06-23 21:04:29 +0300

    Create AddPlaylistActivity.kt

---
+++ app/src/main/java/com/media/iptvplayer/AddPlaylistActivity.kt
@@ -0,0 +1,13 @@
+package com.media.iptvplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddPlaylistActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_add_playlist)
+    }
+}

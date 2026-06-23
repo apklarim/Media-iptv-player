845ba10d3935dab12d5d33debcc73f41e53904db
apklarim <apklarim@gmail.com>
2026-06-24 00:10:07 +0300

    Update NetworkUtils.kt

--- app/src/main/java/com/media/iptvplayer/NetworkUtils.kt
+++ app/src/main/java/com/media/iptvplayer/NetworkUtils.kt
@@ -1,14 +1,26 @@
 package com.media.iptvplayer
 
+import java.net.HttpURLConnection
 import java.net.URL
 
 object NetworkUtils {
 
     fun downloadText(url: String): String {
 
-        return URL(url)
-            .openConnection()
-            .getInputStream()
+        val connection =
+            URL(url).openConnection() as HttpURLConnection
+
+        connection.connectTimeout = 15000
+        connection.readTimeout = 15000
+        connection.requestMethod = "GET"
+        connection.setRequestProperty(
+            "User-Agent",
+            "Mozilla/5.0"
+        )
+
+        connection.connect()
+
+        return connection.inputStream
             .bufferedReader()
             .use { it.readText() }
     }

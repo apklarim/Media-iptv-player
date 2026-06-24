b46c8b4da6e73a908c086d287de1d2ee5c189650
apklarim <apklarim@gmail.com>
2026-06-24 19:41:49 +0300

    Update PlayerActivity.kt

--- app/src/main/java/com/media/iptvplayer/PlayerActivity.kt
+++ app/src/main/java/com/media/iptvplayer/PlayerActivity.kt
@@ -2,11 +2,8 @@ package com.media.iptvplayer
 
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.MediaItem
-import androidx.media3.common.PlaybackException
-import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
@@ -23,56 +20,36 @@ class PlayerActivity : AppCompatActivity() {
 
         playerView = findViewById(R.id.playerView)
 
-        val url = intent.getStringExtra("url")
-
-        if (url.isNullOrEmpty()) {
-
-            Toast.makeText(
-                this,
-                "URL bulunamadı",
-                Toast.LENGTH_LONG
-            ).show()
-
-            finish()
-            return
-        }
-
-        Toast.makeText(
-            this,
-            "URL alındı",
-            Toast.LENGTH_SHORT
-        ).show()
+        val url = intent.getStringExtra("url") ?: return
 
         player = ExoPlayer.Builder(this)
-            .setSeekBackIncrementMs(10000)
-            .setSeekForwardIncrementMs(10000)
             .build()
 
         playerView.player = player
 
-        player.addListener(object : Player.Listener {
-
-            override fun onPlayerError(
-                error: PlaybackException
-            ) {
-
-                Toast.makeText(
-                    this@PlayerActivity,
-                    "Player Hatası: ${error.message}",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        })
-
-        val mediaItem = MediaItem.fromUri(
-            Uri.parse(url)
-        )
+        val mediaItem = MediaItem.fromUri(Uri.parse(url))
 
         player.setMediaItem(mediaItem)
 
         player.prepare()
 
         player.playWhenReady = true
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (::player.isInitialized) {
+            player.play()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (::player.isInitialized) {
+            player.pause()
+        }
     }
 
     override fun onDestroy() {

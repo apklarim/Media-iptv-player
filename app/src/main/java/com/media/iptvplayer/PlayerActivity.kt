3601e93b3e09c087cc31b58973d13560a004cab2
apklarim <apklarim@gmail.com>
2026-06-24 20:12:59 +0300

    Update PlayerActivity.kt

--- app/src/main/java/com/media/iptvplayer/PlayerActivity.kt
+++ app/src/main/java/com/media/iptvplayer/PlayerActivity.kt
@@ -2,11 +2,8 @@ package com.media.iptvplayer
 
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.MediaItem
-import androidx.media3.common.PlaybackException
-import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
@@ -26,43 +23,25 @@ class PlayerActivity : AppCompatActivity() {
         val url = intent.getStringExtra("url")
 
         if (url.isNullOrEmpty()) {
-
-            Toast.makeText(
-                this,
-                "URL bulunamadı",
-                Toast.LENGTH_LONG
-            ).show()
-
             finish()
             return
         }
 
-        Toast.makeText(
-            this,
-            url,
-            Toast.LENGTH_LONG
-        ).show()
-
-        player = ExoPlayer.Builder(this).build()
+        player = ExoPlayer.Builder(this)
+            .setSeekBackIncrementMs(10000)
+            .setSeekForwardIncrementMs(10000)
+            .build()
 
         playerView.player = player
 
-        player.addListener(object : Player.Listener {
+        playerView.setShowNextButton(false)
+        playerView.setShowPreviousButton(false)
+        playerView.setShowRewindButton(true)
+        playerView.setShowFastForwardButton(true)
 
-            override fun onPlayerError(
-                error: PlaybackException
-            ) {
-
-                Toast.makeText(
-                    this@PlayerActivity,
-                    "Hata: ${error.errorCodeName}\n${error.message}",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        })
-
-        val mediaItem =
-            MediaItem.fromUri(Uri.parse(url))
+        val mediaItem = MediaItem.fromUri(
+            Uri.parse(url)
+        )
 
         player.setMediaItem(mediaItem)
 
